0791c2357f5f5afb0cdf06087e992c381aa004b8
theabore-max <theabore@gmail.com>
2025-09-24 14:17:43 +0200

    v3.4

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import AuthForm from "./components/AuthForm";
 import EventList from "./components/EventList";
 import AddEventPage from "./pages/AddEventPage";
-import { useauth } from "./hooks/useauth";
+//import { useauth } from "./hooks/useauth";
 
 function App() {
-  const user = useauth();
+  //const user = useauth();
 
   return (
     <Router>
