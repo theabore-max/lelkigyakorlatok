bc09f3a08f61695396d9ee33be6196de92839858
theabore-max <theabore@gmail.com>
2025-09-26 20:54:33 +0200

    v6.75

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import { supabase } from "../supabaseClient";
 import headerImage from "../assets/header.jpg";
-import placeholderImage from "../card_1.jpg";
+import placeholderImage from "../assets/card_1.jpg";
 
 export default function EventList({ user }) {
   const [events, setEvents] = useState([]);
